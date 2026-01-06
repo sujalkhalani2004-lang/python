bec3a287356263bc2030450657538d2982866b30
Sujal <sujal@gmail.com>
2026-01-06 17:20:09 +0530

    inisialize python repository

---
+++ Hello.py
@@ -0,0 +1,38 @@
+# print("hello word")
+
+# 1
+# name = input("Enter your name:")
+# age = int(input("Enter your age:"))
+# rollno =  int(input("Enter your rollno:"))
+# print("your name:", name)
+# print("your age:", age)
+# print("your rollno:", rollno)
+
+# 2
+# print 2 number sum
+# num1 = int(input("Enter number 1:"))
+# num2 = int(input("Enter number 2:"))
+# sum = num1 + num2
+# print("Sum is:", sum)
+
+# 3
+# input side of square print its area
+
+# a = float(input("Enter side area sequre:"))
+# print("area is ", a * a)
+
+
+# 4
+
+# 2 number avg
+# A = float(input("Enter number one :"))
+# B = float(input("Enter number second:"))
+# avg = (A+B)/2
+# print("avg is ",avg)
+
+# 5
+
+A = int(input("Enter number one :"))
+B = int(input("Enter number second:"))
+
+print(A >= B)
---
+++ strig&Conditional .py
@@ -0,0 +1,81 @@
+# string function 
+
+# endwith
+# str1 = "hello my name is sujal hello"
+# print(str1.endswith("is"))
+
+# # capitalize
+# print(str1.capitalize())
+# print(str1)
+
+# # replace 
+# print(str1.replace("a","A"))
+
+# #find 
+# print(str1.find("a"))
+
+# # count
+# print(str1.count("hello"))
+
+# exersice
+
+# input name and chack len
+# Name = input("Enter your name:")
+# print("Your name len is :",len(Name))
+
+# count a word in sentence
+
+# Name = "this is my name is sujal and i willl work in microsoft compny than 5 lakh per month sallry"
+# print(Name.count("i"))
+
+# student mark give gread
+# marks =int( input("Enter marks:") )
+
+# if(marks >=90):
+#     gread = "A"
+# elif(marks >= 80 and marks < 90):
+#     gread = "B"
+# elif(marks >= 70 and marks < 80):
+#     gread = "C"
+# else:
+#     gread = "D"
+
+# print("The gread is a",gread)
+
+# trafic light
+# light = input("Enter light:")
+# if(light == "Green"):
+#     print("Go Go")
+# elif(light == "red"):
+#     print("Stop")
+# elif(light == "yellow"):
+#     print("Wornig")
+# else:
+#     print("wrong light re-enter light")
+
+# user input odd ya even
+# n= int(input("enter number:"))
+
+# if(n%2==0):
+#     print("Even number")
+# else:
+#     print("odd number")
+
+# user enter 3 number gretest number print
+# a=int(input("Enter first number:"))
+# b=int(input("Enter second number:"))
+# c=int(input("Enter third number:"))
+
+# if(a>=b and a>=c):
+#     print("First is largest")
+# elif(b>=c):
+#     print("Second is largest")
+# else:
+#     print("third is largest ")
+
+# multi 7 
+# x = int(input("Enter number"))
+# if(x%7==0):
+#     print("multipal of 7")
+# else:
+#     print("not a multipal")
